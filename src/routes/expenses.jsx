6b4be533102c5642cad9f6e8523be40874a5a32e
anghel7 <ghehahanghel@gmail.com>
2021-12-01 15:52:14 -0400

    1 file modified

--- src/routes/expenses.jsx
+++ src/routes/expenses.jsx
@@ -2,6 +2,10 @@ export default function Expenses() {
   return (
     <main style={{ padding: '1rem 0' }}>
       <h2>Expenses</h2>
+      <img
+        src="https://cdn.wallstreetmojo.com/wp-content/uploads/2020/02/Business-Expenses-main.jpg"
+        alt="expenses_image"
+      />
     </main>
   );
 }
